cd4bd4e10f4202354d939a0023b7932b0d6f81a1
antonin archer <antonin.archer@hec.edu>
2015-08-18 16:26:28 +0200

    seed populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'faker'
+10.times do
+  Article.create(title: Faker::App.name, content: Faker::Lorem.sentences)
+end
