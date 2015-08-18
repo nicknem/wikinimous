dd0d0d697239d913f5154e1abc356f8a7d6d2172
antonin archer <antonin.archer@hec.edu>
2015-08-18 17:22:05 +0200

    invalid authenticity error on NEW

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,16 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(params[:article])
+    @article.save
   end
 
   def edit
